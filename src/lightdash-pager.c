3f7396d525cde1bc0333304fc25091b1f4f35316
adlo <adloconwy@gmail.com>
2015-11-01 02:24:13 +0000

    Initial work on new pager widget

--- src/lightdash-pager.c
+++ src/lightdash-pager.c
@@ -23,3 +23,36 @@ struct _LightdashPagerPrivate
 	WnckScreen *screen;
 };
 
+#define LIGHTDASH_PAGER_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), LIGHTDASH_PAGER_TYPE, LightdashPagerPrivate))
+
+static void lightdash_pager_init (LightdashPager *pager)
+{
+	static const GtkTargetEntry targets [] = {
+			{"application/x-wnck-window-id", 0, 0}
+		};
+		
+	pager->priv = LIGHTDASH_PAGER_GET_PRIVATE (pager);
+}
+
+static void lightdash_pager_class_init (LightdashPagerClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+	
+	g_type_class_add_private (klass, sizeof (LightdashPagerPrivate));
+	
+}
+
+#if GTK_CHECK_VERSION (3, 0, 0)
+static gboolean lightdash_pager_draw (GtkWidget *widget, cairo_t *cr)
+#else
+static gboolean lightdash_pager_expose_event (GtkWidget *widget, GdkEventExpose *event)
+#endif
+{
+	
+}
+
+GtkWidget * lightdash_pager_new ()
+{
+	return GTK_WIDGET(g_object_new (lightdash_pager_get_type (), NULL));
+}
