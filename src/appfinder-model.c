1171953b31925cfacf7bd824632838a4d21cf503
adlo <adloconwy@gmail.com>
2016-01-03 19:16:11 +0000

    Update year to 2016

--- src/appfinder-model.c
+++ src/appfinder-model.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011-2013 Nick Schermer <nick@xfce.org>
+ * Copyright (C) 2015 adlo
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
