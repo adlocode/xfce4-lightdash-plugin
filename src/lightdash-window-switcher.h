406af8cd22745cd1535e9f92754aa2873dda5fe0
adlo <adloconwy@gmail.com>
2016-01-07 18:54:00 +0000

    lightdash-windows-view: Rename and remove things

--- src/lightdash-window-switcher.h
+++ src/lightdash-window-switcher.h
@@ -14,8 +14,8 @@
  * along with this library; if not, see <http://www.gnu.org/licenses/>
  */
  
-#ifndef MY_TASKLIST_H
-#define MY_TASKLIST_H
+#ifndef LIGHTDASH_WINDOWS_VIEW_H
+#define LIGHTDASH_WINDOWS_VIEW_H
 
 #include <glib.h>
 #include <glib-object.h>
@@ -28,10 +28,10 @@
 G_BEGIN_DECLS
 
 #define LIGHTDASH_WINDOWS_VIEW_TYPE (lightdash_windows_view_get_type())
-#define LIGHTDASH_WINDOWS_VIEW(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), LIGHTDASH_WINDOWS_VIEW_TYPE, MyTasklist))
-#define MY_TASKLIST_CLASS (klass) (G_TYPE_CHECK_CLASS_CAST ((klass), LIGHTDASH_WINDOWS_VIEW_TYPE, MyTasklistClass))
-#define IS_MY_TASKLIST (obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), LIGHTDASH_WINDOWS_VIEW_TYPE))
-#define IS_MY_TASKLIST_CLASS(klass), (G_TYPE_CHECK_CLASS_TYPE ((klass), LIGHTDASH_WINDOWS_VIEW_TYPE))
+#define LIGHTDASH_WINDOWS_VIEW(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), LIGHTDASH_WINDOWS_VIEW_TYPE, LightdashWindowsView))
+#define MY_TASKLIST_CLASS (klass) (G_TYPE_CHECK_CLASS_CAST ((klass), LIGHTDASH_WINDOWS_VIEW_TYPE, LightdashWindowsViewClass))
+#define IS_LIGHTDASH_WINDOWS_VIEW (obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), LIGHTDASH_WINDOWS_VIEW_TYPE))
+#define IS_LIGHTDASH_WINDOWS_VIEW_CLASS(klass), (G_TYPE_CHECK_CLASS_TYPE ((klass), LIGHTDASH_WINDOWS_VIEW_TYPE))
 
 typedef struct _MyTasklist LightdashWindowsView;
 typedef struct _MyTasklistClass LightdashWindowsViewClass;
@@ -68,11 +68,6 @@ struct _MyTasklist
 	guint table_columns;
 	guint table_rows;
 	
-	GHashTable *win_hash;
-	
-	
-	
-	
 };
 
 
@@ -89,4 +84,4 @@ GtkWidget* lightdash_window_switcher_new (void);
 
 G_END_DECLS
 
-#endif /*__MY_TASKLIST_H__*/
+#endif /*LIGHTDASH_WINDOWS_VIEW_H*/
